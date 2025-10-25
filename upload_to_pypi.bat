93587c4b5a0a68518b34b92201464807b2f0bc67
MinchinWeb <w_minchin@hotmail.com>
2025-10-24 23:09:06 -0600

    Swap from `setup.py` to `pyproject.toml`
    c.f. https://github.com/minchinweb/minchin.pelican.plugins.autoloader/pull/2
    
    Builds on / depends on #8

--- upload_to_pypi.bat
+++
@@ -1,8 +0,0 @@
-REM Assumes is called from the root directory!
-REM update pip, setuptools, wheel, twine
-python -m pip install pip -U
-pip install setuptools wheel twine -U
-REM create distributions
-python setup.py sdist bdist_egg bdsit_wheel
-REM upload!
-twine upload dist/*
